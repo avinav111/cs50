e69fcfe172dfdaa138d3267874446dd4531bce38
avinav111 <37254844+avinav111@users.noreply.github.com>
2020-07-03 01:14:46 -0400

    Update credit.c

--- credit.c
+++ credit.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 int length(long number);
-
+bool checksum(long number);
+bool valid(long number);
+void card(long number);
 int main(void)
 {
     long number;
@@ -10,8 +12,14 @@ int main(void)
         number = get_long("Number:\n");
     }while(number<0);
 
-    printf("%i\n",length(number));
-
+    if(valid(number)==true)
+    {
+        card(number);
+    }
+  //  else
+ //   {
+  //      printf("INVALID\n");
+//    }
 }
 int length(long number)
 {
@@ -24,15 +32,61 @@ int length(long number)
 }
 bool checksum(long number)
 {
-    for(int x=0; number!=0; x++; number/=10)
+    int sum = 0;
+    for(int x=0; number!=0; x++, number/=10)
     {
         if(x%2==0)
         {
-            
+           sum += number % 10;
+//do the algebra now
         }
-        else(x%2==1)
+        else
         {
-            
+            int sum2 = 2*(number%10);
+            sum+=sum2/10 + sum %10;
         }
+    }
+    /*if((sum%10)==0)
+    {
+       return true;
+    }
+    else
+    {
+        return false;
+    }
+    */
+    return (sum%10)==0;
+}
+bool valid(long number)
+{
+    int length2= length(number);
+    return (length2 == 13 || length2 == 15|| length2 == 16)&& (checksum(number));
+    /*if((length2 == 13 || length2 == 15|| length2 == 16)&& (checksum(number)==true))
+    {
+         return true;
+    }
+    else
+    {
+        return false;
+    }
+*/
+}
+void card(long number)
+{
+    if((number< 35e13 && number >= 34e13 )|| (number<38e13 && number>=37e13))
+    {
+        printf("AMEX\n");
+    }
+    else if((number< 56e14 && number >= 51e14 ))
+    {
+        printf("MASTERCARD\n");
+    }
+    else if((number< 5*(10^12) && number >= 4*(10^12) )|| (number<5*(10^15) && number>=4*(10^15)))
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
     }
 }
